5fffbe9b4a042e513f04b655a5dceb53c80ce931
Brian Jester <brian.jester@pvmit.com>
2024-10-06 16:44:05 -0700

    Implement logging aspect

--- lab/22-aop/src/main/java/rewards/internal/aspects/LoggingAspect.java
+++ lab/22-aop/src/main/java/rewards/internal/aspects/LoggingAspect.java
@@ -3,20 +3,25 @@ package rewards.internal.aspects;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import rewards.internal.monitor.Monitor;
 import rewards.internal.monitor.MonitorFactory;
 
-// 	TODO-02: Use AOP to log a message before
+// 	xTODO-02: Use AOP to log a message before
 //           any repository's find...() method is invoked.
 //  - Add an appropriate annotation to this class to indicate this class is an aspect.
 //	- Also make it as a component.
 //	- Optionally place @Autowired annotation on the constructor
 //    where `MonitorFactory` dependency is being injected.
 //    (It is optional since there is only a single constructor in the class.)
-
+@Aspect
+@Component
 public class LoggingAspect {
     public final static String BEFORE = "'Before'";
     public final static String AROUND = "'Around'";
@@ -24,18 +29,18 @@ public class LoggingAspect {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	private MonitorFactory monitorFactory;
 
-	
+	@Autowired
 	public LoggingAspect(MonitorFactory monitorFactory) {
 		super();
 		this.monitorFactory = monitorFactory;
 	}
 
 
-	// TODO-03: Write Pointcut Expression
+	// xTODO-03: Write Pointcut Expression
 	// - Decide which advice type is most appropriate
 	// - Write a pointcut expression that selects only find* methods on
 	//    our repository classes.
-
+	@Before("execution(* *.find*(*))")
 	public void implLogging(JoinPoint joinPoint) {
 		// Do not modify this log message or the test will fail
 		logger.info(BEFORE + " advice implementation - " + joinPoint.getTarget().getClass() + //
